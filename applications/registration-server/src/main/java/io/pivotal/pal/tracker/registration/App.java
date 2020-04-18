9868cfc0213fd33670c268259927caf4eb4cf112
Uday <udaykanth@outlook.com>
2020-04-18 16:15:42 +0530

    OAuth

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import java.util.TimeZone;
 
@@ -17,6 +20,8 @@ import java.util.TimeZone;
     "io.pivotal.pal.tracker.users",
     "io.pivotal.pal.tracker.registration"
 })
+@EnableWebSecurity
+@EnableResourceServer
 public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
